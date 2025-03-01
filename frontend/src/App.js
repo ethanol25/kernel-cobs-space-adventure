e87f1b5af7d1df0f9773e207405b57247ea18ff5
JacobDDoS <jacdimmitt@gmail.com>
2025-03-01 11:33:47 -0600

    Added Reducer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
+import { useReducer } from 'react';
 import './css/App.css';
 import Display from './pages/GameDisplay';
+import reducer from './data/reducer';
+import initialState from './data/initialState';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <Display/>
+    <Display state={state} dispatch={dispatch}/>
   );
 }
 
